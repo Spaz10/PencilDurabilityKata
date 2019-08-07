e3afd482046b47dcac82bdc69b882531ab22e25e
spaz10 <gweneveirstevens@gmail.com>
2019-08-07 10:34:09 -0400

    add durability to Pencil, test that it is instaniated with a given durability

--- src/main/java/Pencil.java
+++ src/main/java/Pencil.java
@@ -1,5 +1,15 @@
 public class Pencil {
+    private int durability;
+
+    public Pencil(int durability){
+        this.durability = durability;
+    }
+
     public void write(Paper paper, String textToWrite) {
         paper.write(textToWrite);
+    }
+
+    public int getDurability(){
+        return this.durability;
     }
 }
--- src/test/java/PencilTest.java
+++ src/test/java/PencilTest.java
@@ -5,7 +5,7 @@ public class PencilTest {
     @Test
     public void PencilWritesAppleToPaper(){
         Paper paper = new Paper();
-        Pencil pencil = new Pencil();
+        Pencil pencil = new Pencil(5);
         pencil.write(paper, "apple");
 
         assertEquals("apple", paper.getText());
@@ -14,9 +14,15 @@ public class PencilTest {
     @Test
     public void TextWrittenByThePencilShouldAlwaysBeAppendedToExistingTextOnThePaper(){
         Paper paper = new Paper("She sells sea shells");
-        Pencil pencil = new Pencil();
+        Pencil pencil = new Pencil(5);
         pencil.write(paper, " down by the sea shore");
 
         assertEquals("She sells sea shells down by the sea shore", paper.getText());
+    }
+
+    @Test
+    public void APencilIsCreatedWithAGivenDurability(){
+        Pencil pencil = new Pencil(5);
+        assertEquals(5, pencil.getDurability());
     }
 }
